19def009ca2d01c36ced8520ea49aac645fbb091
Luiz Felipe Canário Costa <luizfelipecanariocosta@gmail.com>
2022-05-02 19:09:04 -0300

    °

--- Aula 9/src/Pessoa.java
+++ Aula 9/src/Pessoa.java
@@ -3,7 +3,7 @@ public class Pessoa {
 
 	private String nome;
 	private String cpf;
-	
+
 	public String getNome() {
 		return nome;
 	}
@@ -25,13 +25,13 @@ public class Pessoa {
 		this.nome = nome;
 		this.cpf = cpf;
 	}
-	
+
 	// Serve apenas para ordenação
-		//@Override
-		//public int compareTo(Pessoa outraPessoa) {
-		//	return this.nome.compareTo(outraPessoa.nome);
-		//}
-	
+	// @Override
+	// public int compareTo(Pessoa outraPessoa) {
+	// return this.nome.compareTo(outraPessoa.nome);
+	// }
+
 	@Override
 	public String toString() {
 		return "Pessoa [nome=" + nome + ", cpf=" + cpf + "]";
@@ -40,21 +40,24 @@ public class Pessoa {
 	public int compareTo(Pessoa outraPessoa) {
 		// CompareTo tem uma regra que é usada para qualquer compareTo
 		// Retornamos 0 se os valores são iguais
-		// Retornamos -1 se o primeiro é menor que o segundo 
-		// Retornamos 1 se o primeiro é maior que o segundo 
+		// Retornamos -1 se o primeiro é menor que o segundo
+		// Retornamos 1 se o primeiro é maior que o segundo
 
-		if(this.cpf.equals(outraPessoa.cpf)) {
+		if (this.cpf.equals(outraPessoa.cpf)) {
 			return 0; // Retornar 0, significa que são iguais
 		}
-		// Caso contrário, comparo o nome das pessoas e se o nome da primeira for menor que a segunda
-		else if (this.nome.compareTo(outraPessoa.nome)<0) {
-			return -1; // Retornar -1, significa que o nome atual é anterior no alfabeto em relação a outra Pessoa
-		
+		// Caso contrário, comparo o nome das pessoas e se o nome da primeira for menor
+		// que a segunda
+		else if (this.nome.compareTo(outraPessoa.nome) < 0) {
+			return -1; // Retornar -1, significa que o nome atual é anterior no alfabeto em relação a
+						// outra Pessoa
+
 			// Se não cair nos casos anteriores, retorna que é maior
-		}else {
-			return 1; // Retornar 1, significa que o nome atual é posterior no alfabeto em relação a outra Pessoa.
+		} else {
+			return 1; // Retornar 1, significa que o nome atual é posterior no alfabeto em relação a
+						// outra Pessoa.
 		}
-		
+
 	}
 
 }
---
+++ Sistema Bancário POO/src/contas/ContaCorrente.java
@@ -0,0 +1,11 @@
+package contas;
+
+
+public class ContaCorrente extends Conta {
+
+	public ContaCorrente(String cpf, int idAgencia, double saldo) {
+		super(cpf, idAgencia, saldo);
+
+	}
+
+}
